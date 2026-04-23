eec173ca144542ca910d0a91476e895d9d695afa
bevansj17 <bevansonyjoseph2004@gmail.com>
2026-04-23 11:43:45 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('tatva.html')
+
+@app.route('/tatva')
+def tatva():
+    return render_template('tatva.html')
+
+@app.route('/old')
+def old():
+    return render_template('old.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        message = request.form['message']
+        print(name, phone, message)  # Later store in DB
+        return redirect('/')
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
